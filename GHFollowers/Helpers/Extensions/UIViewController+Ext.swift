be40220000941376e827c33f322626ff778c1833
codedubu <mccaine.river@gmail.com>
2021-07-19 13:29:58 -0700

    Adds empty state view for when users have no followers.

--- GHFollowers/Helpers/Extensions/UIViewController+Ext.swift
+++ GHFollowers/Helpers/Extensions/UIViewController+Ext.swift
@@ -50,5 +50,11 @@ extension UIViewController {
         }
     }
     
+    func showEmptyStateView(with message: String, in view: UIView) {
+        let emptyStateView = GFEmptyStateView(message: message)
+        emptyStateView.frame = view.bounds
+        view.addSubview(emptyStateView)
+    }
+    
 } // END OF EXTENSION
 
